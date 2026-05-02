630010982c279c4d9bc52298a8e7707ae842a336
hyeonryeol <hunyeol138@gmail.com>
2026-05-02 14:49:12 +0900

    기본 배경

--- 텀프로젝트/텀프.cpp
+++ 텀프로젝트/텀프.cpp
@@ -1,4 +1,7 @@
 #include <windows.h>
+#include <gdiplus.h>
+#pragma comment(lib, "gdiplus.lib")
+using namespace Gdiplus;
 
 // 윈도우 프로시저 선언
 LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
@@ -7,9 +10,13 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
 const wchar_t CLASS_NAME[] = L"MyWindowClass";
 const wchar_t WINDOW_TITLE[] = L"Win32 Basic Template";
 
+
 // 프로그램 시작점
 int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE, PWSTR, int nCmdShow)
 {
+    GdiplusStartupInput gdiplusStartupInput;
+    ULONG_PTR gdiplusToken;
+    GdiplusStartup(&gdiplusToken, &gdiplusStartupInput, NULL);
     // 1) 윈도우 클래스 등록
     WNDCLASSEXW wc = {};
     wc.cbSize = sizeof(wc);
@@ -30,7 +37,9 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE, PWSTR, int nCmdShow)
         CLASS_NAME,
         WINDOW_TITLE,
         WS_OVERLAPPEDWINDOW,
-        CW_USEDEFAULT, CW_USEDEFAULT, 800, 600,
+        0, 0,
+        GetSystemMetrics(SM_CXSCREEN),
+        GetSystemMetrics(SM_CYSCREEN),
         nullptr, nullptr, hInstance, nullptr
     );
 
@@ -47,7 +56,7 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE, PWSTR, int nCmdShow)
         TranslateMessage(&msg);
         DispatchMessage(&msg);
     }
-
+    GdiplusShutdown(gdiplusToken);
     return (int)msg.wParam;
 }
 
@@ -65,8 +74,13 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
     {
         PAINTSTRUCT ps;
         HDC hdc = BeginPaint(hWnd, &ps);
-
+        RECT rc;
+        GetClientRect(hWnd, &rc);
+        
         // TODO: 여기에 그리기 코드 작성
+        Graphics graphics(hdc);
+        Image image(L"papers\\배경.png");
+        graphics.DrawImage(&image, (int)rc.left, (int)rc.top, (int)rc.right, (int)rc.bottom);
 
         EndPaint(hWnd, &ps);
         return 0;
