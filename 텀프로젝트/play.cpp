438a9e4d59ef244197ded57288ccbd0fe9a9b457
hyeonryeol <hunyeol138@gmail.com>
2026-05-03 15:57:26 +0900

    파일 이름 수정

--- 텀프로젝트/play.cpp
+++ 텀프로젝트/play.cpp
@@ -3,6 +3,6 @@
 
 void Play::Draw(HDC hdc, RECT rc) {
     Graphics graphics(hdc);
-    Image image(L"papers\\배경.png");  // ← 배경 이미지
+    Image image(L"papers\\background.png");  // ← 배경 이미지
     graphics.DrawImage(&image, (int)rc.left, (int)rc.top, (int)rc.right, (int)rc.bottom);
 }
