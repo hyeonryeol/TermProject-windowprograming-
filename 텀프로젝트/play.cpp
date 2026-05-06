1b6cab2328a3a109d412a899001481cbe2604cdd
hyeonryeol <hunyeol138@gmail.com>
2026-05-06 18:39:19 +0900

    remove

--- 텀프로젝트/play.cpp
+++
@@ -1,8 +0,0 @@
-// Play.cpp
-#include "Play.h"
-
-void Play::Draw(HDC hdc, RECT rc) {
-    Graphics graphics(hdc);
-    Image image(L"papers\\background.png");  // ← 배경 이미지
-    graphics.DrawImage(&image, (int)rc.left, (int)rc.top, (int)rc.right, (int)rc.bottom);
-}
